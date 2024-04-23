47cc94fa9f4de5bd91997feee3107bc6eb669b96
Muhammad Asad <asadghafoor26.figma@gmail.com>
2024-04-23 23:11:31 +0500

    navbar updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-    <Navbar></Navbar>
+      <div className="app">
+        <Navbar />
+      </div>
     </>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,28 @@
-import logo from '../../Assets/logo.png';
-
-function Navbar()
-{
-    return (
-        <>
-        <div className="navbar">
-            <img src={logo} alt="logo" className="logo" />
+import logo from "../../Assets/logo.png";
+import search_icon from "../../Assets/search_icon.png";
+import basket_icon from "../../Assets/basket_icon.png";
+import '../Navbar/Navbar.css';
+function Navbar() {
+  return (
+    <>
+      <div className="navbar">
+        <img src={logo} alt="logo" className="logo" />
+        <ul className="navbar-menu">
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Mobile App</li>
+          <li>Contact Us</li>
+        </ul>
+        <div className="navbar-right">
+          <img src={search_icon} alt="" />
+          <div className="navbar-search-icon">
+            <img src={basket_icon} alt="" />
+            <div className="dot"></div>
+          </div>
+          <button>sign in</button>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 export default Navbar;
