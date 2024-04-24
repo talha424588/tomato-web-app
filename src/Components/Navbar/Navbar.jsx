f5a5b54eed4780cca85390fb02c5fef041f3d4e5
Muhammad Asad <asadghafoor26.figma@gmail.com>
2024-04-24 07:09:32 +0500

    navbar complete

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,16 +2,18 @@ import logo from "../../Assets/logo.png";
 import search_icon from "../../Assets/search_icon.png";
 import basket_icon from "../../Assets/basket_icon.png";
 import '../Navbar/Navbar.css';
+import { useState } from "react";
 function Navbar() {
+    const [activeNav, setActiveNav] = useState("#")
   return (
     <>
       <div className="navbar">
         <img src={logo} alt="logo" className="logo" />
         <ul className="navbar-menu">
-          <li>Home</li>
-          <li>Menu</li>
-          <li>Mobile App</li>
-          <li>Contact Us</li>
+          <li onClick={()=>setActiveNav("#")} className={activeNav === "#" ? "active":""}>Home</li>
+          <li onClick={()=>setActiveNav("menu")} className={activeNav === "menu" ? "active" :""}>Menu</li>
+          <li onClick={()=>setActiveNav("mobile-app")} className={activeNav === "mobile-app" ? "active" : ""}>Mobile App</li>
+          <li onClick={()=>setActiveNav("contact-us")} className={activeNav=== "contact-us" ? "active" : ""}>Contact Us</li>
         </ul>
         <div className="navbar-right">
           <img src={search_icon} alt="" />
