97f4e500686aadea0702854e6456deae26d0ff11
Muhammad Talha <talha424588@gmail.com>
2024-04-25 23:52:58 +0500

    menu addded

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,13 +1,18 @@
 import "./Menu.css"
 import { menu_list } from "../../Assets/assets";
-function Menu()
+function  Menu()
 {
-    console.log("menu list",menu_list);;
+    // console.log("menu list",menu_list);;
     return (
         <>
         <div className="explore-menu" id="explore-menu">
             <h1>Explore our menu</h1>
-            <p>Choose from a di</p>
+            <p className="explore-text-menu">Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredidient and culinary experience</p>
+            <div className="explore-menu-list">
+                {
+
+                }
+            </div>
         </div>
         </>
     )
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Header from '../../Components/Header/Header';
+import Menu from '../../Components/Menu/Menu';
 import './Home.css'
 function Home()
 {
     return (
         <>
         <Header></Header>
+        <Menu/>
         </>
     )
 }
