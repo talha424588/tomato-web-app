1cfc5a75957984dcaa04dae27834d601c2265953
Muhammad Talha <talha424588@gmail.com>
2024-04-25 12:23:50 +0500

    header image animation removed & menu list pending

---
+++ src/Components/Menu/Menu.jsx
@@ -0,0 +1,15 @@
+import "./Menu.css"
+import { menu_list } from "../../Assets/assets";
+function Menu()
+{
+    console.log("menu list",menu_list);;
+    return (
+        <>
+        <div className="explore-menu" id="explore-menu">
+            <h1>Explore our menu</h1>
+            <p>Choose from a di</p>
+        </div>
+        </>
+    )
+}
+export default Menu;
