3f223f239875fa83e655b78305631083a157ee48
Muhammad Talha <talha424588@gmail.com>
2024-04-26 08:00:49 +0500

    menu item listed

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,21 +1,19 @@
 import "./Menu.css";
 import { menu_list } from "../../Assets/assets";
-function Menu() {
-  // console.log("menu list",menu_list);;
+function Menu({ categrory, setCategory }) {
   return (
     <>
       <div className="explore-menu" id="explore-menu">
         <h1>Explore our menu</h1>
         <p className="explore-text-menu">
-          Choose from a diverse menu featuring a delectable array of dishes.Our mission is to satify your craving and elevate your dining experience, one delicious at a time.
+          Choose from a diverse menu featuring a delectable array of dishes.Our
+          mission is to satify your craving and elevate your dining experience,
+          one delicious at a time.
         </p>
         <div className="explore-menu-list">
-          {menu_list.map((item,index) => (
-            <div key={index} className="explore-menu-list-item">
-              <img
-                src={item.menu_image}
-                alt="menu"
-              />
+          {menu_list.map((item, index) => (
+            <div onClick={()=>setCategory(prev => prev === item.menu_name? "all" : item.menu_name)} key={index} className="explore-menu-list-item">
+              <img className={categrory === item.menu_name? "active":""} src={item.menu_image} alt="menu" />
               <p>{item.menu_name}</p>
             </div>
           ))}
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import Header from '../../Components/Header/Header';
 import Menu from '../../Components/Menu/Menu';
 import './Home.css'
 function Home()
 {
+    const [categroy, setCategory] = useState("all");
+    console.log("category",categroy);
+
     return (
         <>
         <Header></Header>
-        <Menu/>
+        <Menu categroy={categroy} setCategory={setCategory}/>
         </>
     )
 }
