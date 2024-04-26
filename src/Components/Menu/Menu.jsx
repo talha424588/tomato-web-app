a8452df55e26c7576ccdf41cfb1909259acf87df
Muhammad Talha <talha424588@gmail.com>
2024-04-26 06:29:58 +0500

    header complete

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,20 +1,28 @@
-import "./Menu.css"
+import "./Menu.css";
 import { menu_list } from "../../Assets/assets";
-function  Menu()
-{
-    // console.log("menu list",menu_list);;
-    return (
-        <>
-        <div className="explore-menu" id="explore-menu">
-            <h1>Explore our menu</h1>
-            <p className="explore-text-menu">Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredidient and culinary experience</p>
-            <div className="explore-menu-list">
-                {
-
-                }
+function Menu() {
+  // console.log("menu list",menu_list);;
+  return (
+    <>
+      <div className="explore-menu" id="explore-menu">
+        <h1>Explore our menu</h1>
+        <p className="explore-text-menu">
+          Choose from a diverse menu featuring a delectable array of dishes.Our mission is to satify your craving and elevate your dining experience, one delicious at a time.
+        </p>
+        <div className="explore-menu-list">
+          {menu_list.map((item,index) => (
+            <div key={index} className="explore-menu-list-item">
+              <img
+                src={item.menu_image}
+                alt="menu"
+              />
+              <p>{item.menu_name}</p>
             </div>
+          ))}
         </div>
-        </>
-    )
+        <hr />
+      </div>
+    </>
+  );
 }
 export default Menu;
