de32ff749a5c030f403103768e7bfdb40678ff80
Muhammad Talha <talha424588@gmail.com>
2024-05-01 10:35:49 +0500

    media query added

--- src/Page/Footer/Footer.jsx
+++ src/Page/Footer/Footer.jsx
@@ -35,7 +35,7 @@ function Footer() {
           </div>
         </div>
         <hr />
-        <p className="footer-content">
+        <p className="footer-copyright">
           {" "}
           Copyright 2024 Tomato.com - All Right Reserved
         </p>
