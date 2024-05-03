c7915c161a3e71838ecd36dccfcfc6e3c9e19f69
Muhammad Talha <talha424588@gmail.com>
2024-05-03 10:57:36 +0500

    firebase configuration added

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEF7ATUgiA1br0cTzKEVyWlBUnk2fCyN4",
+  authDomain: "tomato-app-11b90.firebaseapp.com",
+  projectId: "tomato-app-11b90",
+  storageBucket: "tomato-app-11b90.appspot.com",
+  messagingSenderId: "635122645266",
+  appId: "1:635122645266:web:7d7b441f67e2e1c918dbcb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
